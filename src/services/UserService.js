321fd5e975a8dbc49f6f7d0a5d38a57b09d93812
Jared Gonzalez <al222310412@gmail.com>
2025-02-22 12:13:22 -0600

    Soolucion

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,4 +1,9 @@
-const BASE_URL = "http://34.239.105.41/users/";
+const response = await fetch("http://34.239.105.41:8000/users/", {
+    mode: 'no-cors',  // Deshabilita CORS
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
 // Obtener todos los usuarios
 export async function getAllUsers() {
